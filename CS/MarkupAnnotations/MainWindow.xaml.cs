71fc7ce0848a88e3da71080d8676ab597d8dd17c
geumjaLee <polina.fedorova@devexpress.com>
2020-10-30 13:53:33 +0300

    add markup settings

--- CS/MarkupAnnotations/MainWindow.xaml.cs
+++ CS/MarkupAnnotations/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using DevExpress.Pdf;
+using DevExpress.Xpf.DocumentViewer;
 using DevExpress.Xpf.PdfViewer;
 using System.Windows;
 
@@ -26,19 +27,21 @@ namespace MarkupAnnotations
                 IPdfViewerMarkupAnnotationBuilder annotationBuilder = e.Builder.AsMarkupAnnotationBuilder();
 
                 annotationBuilder.Author = "John Smith";
-                annotationBuilder.Contents = "Note.";
-                annotationBuilder.Color = new PdfRGBColor(0.69, 0.75, 0.10);
+                annotationBuilder.Contents = "Note";
+                annotationBuilder.Color = new PdfRGBColor(0.23, 0.48, 0.34);
             }
         }
 
         private void viewer_DocumentLoaded(object sender, RoutedEventArgs e)
         {
+            // Select the text to highlight:
+            viewer.FindText(new TextSearchParameter { Text = "PDF Viewer" });
 
-            // Select the document area where the markup annotation will be created.
-            viewer.Select(new PdfDocumentArea(1, new PdfRectangle(90, 100, 240, 230)));
-
-            // Highlight a selected text.
+            // Highlight selected text:
             viewer.HighlightSelectedText();
+
+            //Add a sticky note:
+            viewer.AddStickyNote(new PdfDocumentPosition(1, new PdfPoint(29, 568)), "Comment");
         }
     }
 }
