9eca04f020416333c0b4129de59b8d2c3922daee
DevExpressExampleBot <fakeEmail>
2024-12-10 15:54:14 +0400

    Source auto update [skip ci]

--- VB/MarkupAnnotations/MainWindow.xaml.vb
+++ VB/MarkupAnnotations/MainWindow.xaml.vb
@@ -1,40 +1,37 @@
-﻿Imports DevExpress.Pdf
+Imports DevExpress.Pdf
+Imports DevExpress.Xpf.DocumentViewer
 Imports DevExpress.Xpf.PdfViewer
 Imports System.Windows
 
 Namespace MarkupAnnotations
 
-	Partial Public Class MainWindow
-		Inherits DevExpress.Xpf.Core.ThemedWindow
+    Public Partial Class MainWindow
+        Inherits DevExpress.Xpf.Core.ThemedWindow
 
-		Public Sub New()
-			InitializeComponent()
+        Public Sub New()
+            Me.InitializeComponent()
+            ' Load a document.
+            Me.viewer.OpenDocument("..\..\Demo.pdf")
+            ' Handle the TextMarkupAnnotationCreating event to specify the annotation properties.
+            AddHandler Me.viewer.AnnotationCreating, AddressOf Me.Viewer_TextMarkupAnnotationCreating
+        End Sub
 
-			' Load a document.
-			viewer.OpenDocument("..\..\Demo.pdf")
+        Private Sub Viewer_TextMarkupAnnotationCreating(ByVal d As DependencyObject, ByVal e As PdfAnnotationCreatingEventArgs)
+            If e.Builder.AnnotationType = PdfAnnotationType.Text OrElse e.Builder.AnnotationType = PdfAnnotationType.TextMarkup Then
+                Dim annotationBuilder As IPdfViewerMarkupAnnotationBuilder = e.Builder.AsMarkupAnnotationBuilder()
+                annotationBuilder.Author = "John Smith"
+                annotationBuilder.Contents = "Note"
+                annotationBuilder.Color = New PdfRGBColor(0.23, 0.48, 0.34)
+            End If
+        End Sub
 
-			' Handle the TextMarkupAnnotationCreating event to specify the annotation properties.
-			AddHandler viewer.AnnotationCreating, AddressOf Viewer_TextMarkupAnnotationCreating
-
-		End Sub
-
-		Private Sub Viewer_TextMarkupAnnotationCreating(ByVal d As DependencyObject, ByVal e As PdfAnnotationCreatingEventArgs)
-			If e.Builder.AnnotationType = PdfAnnotationType.Text OrElse e.Builder.AnnotationType = PdfAnnotationType.TextMarkup Then
-				Dim annotationBuilder As IPdfViewerMarkupAnnotationBuilder = e.Builder.AsMarkupAnnotationBuilder()
-
-				annotationBuilder.Author = "John Smith"
-				annotationBuilder.Contents = "Note."
-				annotationBuilder.Color = New PdfRGBColor(0.69, 0.75, 0.10)
-			End If
-		End Sub
-
-		Private Sub viewer_DocumentLoaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
-
-			' Select the document area where the markup annotation will be created.
-			viewer.Select(New PdfDocumentArea(1, New PdfRectangle(90, 100, 240, 230)))
-
-			' Highlight a selected text.
-			viewer.HighlightSelectedText()
-		End Sub
-	End Class
+        Private Sub viewer_DocumentLoaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            ' Select the text to highlight:
+            Me.viewer.FindText(New TextSearchParameter With {.Text = "PDF Viewer"})
+            ' Highlight selected text:
+            Me.viewer.HighlightSelectedText()
+            'Add a sticky note:
+            Me.viewer.AddStickyNote(New PdfDocumentPosition(1, New PdfPoint(29, 568)), "Comment")
+        End Sub
+    End Class
 End Namespace
