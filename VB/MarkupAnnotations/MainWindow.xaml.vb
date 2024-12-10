aea688fb1f912e7d1b4a87a1c2a361a081dc67c7
DevExpressExampleBot <fakeEmail>
2024-12-10 15:49:57 +0400

    Source auto update [skip ci]

--- VB/MarkupAnnotations/MainWindow.xaml.vb
+++ VB/MarkupAnnotations/MainWindow.xaml.vb
@@ -1,36 +1,31 @@
-﻿Imports System.Windows
+Imports System.Windows
 Imports DevExpress.Pdf
 Imports DevExpress.Xpf.PdfViewer
 
 Namespace MarkupAnnotations
 
-	Partial Public Class MainWindow
-		Inherits Window
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
+        Public Sub New()
+            Me.InitializeComponent()
+            ' Load a document.
+            Me.viewer.OpenDocument("..\..\Demo.pdf")
+            ' Handle the TextMarkupAnnotationCreating event to specify the annotation properties.
+            AddHandler Me.viewer.TextMarkupAnnotationCreating, AddressOf Me.Viewer_TextMarkupAnnotationCreating
+        End Sub
 
-			' Load a document.
-			viewer.OpenDocument("..\..\Demo.pdf")
+        Private Sub Viewer_TextMarkupAnnotationCreating(ByVal d As DependencyObject, ByVal e As PdfTextMarkupAnnotationCreatingEventArgs)
+            e.Author = "John Smith"
+            e.Comment = "Note."
+            e.Color = Media.Color.FromRgb(229, 214, 0)
+        End Sub
 
-			' Handle the TextMarkupAnnotationCreating event to specify the annotation properties.
-			AddHandler viewer.TextMarkupAnnotationCreating, AddressOf Viewer_TextMarkupAnnotationCreating
-
-		End Sub
-
-		Private Sub Viewer_TextMarkupAnnotationCreating(ByVal d As DependencyObject, ByVal e As PdfTextMarkupAnnotationCreatingEventArgs)
-			e.Author = "John Smith"
-			e.Comment = "Note."
-			e.Color = System.Windows.Media.Color.FromRgb(229, 214, 0)
-		End Sub
-
-		Private Sub viewer_DocumentLoaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
-
-			' Select the document area where the markup annotation will be created.
-			viewer.Select(New PdfDocumentArea(1, New PdfRectangle(90, 100, 240, 230)))
-
-			' Highlight a selected text.
-			viewer.HighlightSelectedText()
-		End Sub
-	End Class
+        Private Sub viewer_DocumentLoaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            ' Select the document area where the markup annotation will be created.
+            Me.viewer.Select(New PdfDocumentArea(1, New PdfRectangle(90, 100, 240, 230)))
+            ' Highlight a selected text.
+            Me.viewer.HighlightSelectedText()
+        End Sub
+    End Class
 End Namespace
